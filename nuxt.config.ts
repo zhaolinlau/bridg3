94dd23a30efa6f9521da86dc41bb19311b04a18b
Zhaolin Lau <zhaolinlau@gmail.com>
2024-07-27 00:37:35 +0800

    add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
-	css: ['bulma/css/bulma.min.css']
+	css: ['bulma/css/bulma.min.css'],
+	modules: ["@nuxtjs/supabase"],
+	runtimeConfig: {
+		public: {
+			siteURL: process.env.NUXT_PUBLIC_SITE_URL,
+		},
+	},
 })
